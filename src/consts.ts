008d44035e449d95fd9466e78119beaa3f3acb5d
jonghwanyoon <oknmj101@gmail.com>
2026-02-19 10:30:15 +0900

    design: 인트로 페이지 개편 및 favicon 설정 (Cool & Clean)

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Cosmic Minimalist';
-export const SITE_DESCRIPTION = '우주의 광활함과 미니멀리즘의 단순함을 기록하는 공간';
+export const SITE_TITLE = 'Orbiting Thoughts';
+export const SITE_DESCRIPTION = '코드와 우주, 미니멀리즘의 교차점을 탐구하는 공간';
