65b7f5eceb8177ce811f0283d1ffb41fb6f46463
jonghwanyoon <oknmj101@gmail.com>
2026-09-05 11:30:40 +0900

    Rename blog to Side Notes and keep author name on About

--- src/consts.ts
+++ src/consts.ts
@@ -1,4 +1,4 @@
-export const SITE_TITLE = 'Jonghwan.log';
+export const SITE_TITLE = 'Side Notes';
 export const SITE_DESCRIPTION = '공부한 내용과 읽은 논문을 정리하는 개인 블로그입니다.';
 export const AUTHOR = 'Jonghwan Yoon';
 export const GITHUB_URL = 'https://github.com/jonghwanyoon';
